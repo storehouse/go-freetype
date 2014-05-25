a0701282b968ee45ca87017d0a6dabc27f1196a2
Marcel Hauf <marcel.hauf@googlemail.com>
2014-05-25 22:09:23 +0200

    Add doc to Charmp

--- charmap.go
+++ charmap.go
@@ -14,18 +14,25 @@ type CharMap struct {
 	handle C.FT_CharMap
 }
 
+// A handle to the parent face object.
 func (c *CharMap) Face() Face {
 	return Face{handle: c.handle.face}
 }
 
+// An Encoding tag identifying the charmap.
+// Use this with Face.SelectCharmap.
 func (c *CharMap) Encoding() uint64 {
 	return uint64(c.handle.encoding)
 }
 
+// An ID number describing the platform for the following encoding ID.
+// This comes directly from the TrueType specification and should be emulated for other formats.
 func (c *CharMap) PlatformID() uint16 {
 	return uint16(c.handle.platform_id)
 }
 
+// A platform specific encoding number.
+// This also comes from the TrueType specification and should be emulated similarly.
 func (c *CharMap) EncodingID() uint16 {
 	return uint16(c.handle.encoding_id)
 }
