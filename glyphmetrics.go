bdc6090a9d185ef4bd5b34727e1af1d89d4b5a6e
Marcel Hauf <marcel.hauf@googlemail.com>
2014-05-25 22:04:35 +0200

    Add documentation to GlyphMetrics

--- glyphmetrics.go
+++ glyphmetrics.go
@@ -11,34 +11,44 @@ type GlyphMetrics struct {
 	handle C.FT_Glyph_Metrics
 }
 
+// The glyph's width.
 func (m *GlyphMetrics) Width() int64 {
 	return int64(m.handle.width)
 }
 
+// The glyph's height.
 func (m *GlyphMetrics) Height() int64 {
 	return int64(m.handle.height)
 }
 
+// Left side bearing for horizontal layout.
 func (m *GlyphMetrics) HoriBearingX() int64 {
 	return int64(m.handle.horiBearingX)
 }
 
+// Top side bearing for horizontal layout.
 func (m *GlyphMetrics) HoriBearingY() int64 {
 	return int64(m.handle.horiBearingY)
 }
 
+// Advance width for horizontal layout.
 func (m *GlyphMetrics) HoriAdvance() int64 {
 	return int64(m.handle.horiAdvance)
 }
 
+// Left side bearing for vertical layout.
 func (m *GlyphMetrics) VertBearingX() int64 {
 	return int64(m.handle.vertBearingX)
 }
 
+// Top side bearing for vertical layout.
+// Larger positive values mean further below the vertical glyph origin.
 func (m *GlyphMetrics) VertBearingY() int64 {
 	return int64(m.handle.vertBearingY)
 }
 
+// Advance height for vertical layout.
+// Positive values mean the glyph has a positive advance downward.
 func (m *GlyphMetrics) VertAdvance() int64 {
 	return int64(m.handle.vertAdvance)
 }
