4f06ab29f08569ea6d0c708a12de0a5be80b92c9
Marcel Hauf <marcel.hauf@googlemail.com>
2014-05-26 23:41:49 +0200

    Install ttf-droid fonts on travis test runs. The TestSuite checks a list of font filepaths against the system to find an installed font and uses the first found font file.

--- library_test.go
+++ library_test.go
@@ -2,11 +2,12 @@ package freetype
 
 import (
 	. "gopkg.in/check.v1"
+	"os"
 	"testing"
 )
 
-const (
-	fontFilePathName = "/usr/share/fonts/truetype/DejaVuSans.ttf"
+var (
+	fonts = []string{"/usr/share/fonts/truetype/ttf-droid/DroidSans.ttf", "/usr/share/fonts/truetype/DejaVuSans.ttf"}
 )
 
 func Test(t *testing.T) { TestingT(t) }
@@ -24,6 +25,21 @@ func (s *FreetypeSuite) SetUpSuite(c *C) {
 	c.Assert(err, IsNil)
 	c.Assert(s.lib, Not(IsNil))
 
+	var fontFilePathName string
+
+	for i := range fonts {
+		_, err := os.Open(fonts[i])
+		if err == nil {
+			fontFilePathName = fonts[i]
+			break
+		}
+	}
+
+	if len(fontFilePathName) == 0 {
+		c.Skip("no font file found")
+		return
+	}
+
 	s.face, err = NewFace(s.lib, fontFilePathName, 0)
 	c.Assert(err, IsNil)
 	c.Assert(s.face, Not(IsNil))
@@ -31,8 +47,10 @@ func (s *FreetypeSuite) SetUpSuite(c *C) {
 
 func (s *FreetypeSuite) TearDownSuite(c *C) {
 	var err error
-	err = s.face.Done()
-	c.Assert(err, IsNil)
+	if s.face != nil {
+		err = s.face.Done()
+		c.Assert(err, IsNil)
+	}
 
 	err = s.lib.Done()
 	c.Assert(err, IsNil)
