fce26b1dec5bf809f7cd1fa660b707b739cffdd6
Marcel Hauf <marcel.hauf@googlemail.com>
2014-06-04 21:35:27 +0200

    Add NewVector, Vector.SetX Vector.SetY functions

--- vector.go
+++ vector.go
@@ -11,6 +11,10 @@ type Vector struct {
 	handle C.FT_Vector
 }
 
+func NewVector() *Vector {
+	return &Vector{C.FT_Vector{}}
+}
+
 func (v *Vector) X() int64 {
 	return int64(v.handle.x)
 }
@@ -18,3 +22,6 @@ func (v *Vector) X() int64 {
 func (v *Vector) Y() int64 {
 	return int64(v.handle.y)
 }
+
+func (v *Vector) SetX(x int64) { v.handle.x = C.FT_Pos(x) }
+func (v *Vector) SetY(y int64) { v.handle.y = C.FT_Pos(y) }
