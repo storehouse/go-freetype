2e01b72a5b7189856cc8b8eb7adbdf30f96572fd
Marcel Hauf <marcel.hauf@googlemail.com>
2014-06-08 21:07:53 +0200

    Add Span and SpanFunc

---
+++ span.go
@@ -0,0 +1,26 @@
+package freetype
+
+/*
+#cgo pkg-config: freetype2
+#include <ft2build.h>
+#include FT_FREETYPE_H
+*/
+import "C"
+
+type SpanFunc func(y, count int, spans []Span, user unsafe.Pointer)
+
+type Span struct {
+	handle C.FT_Span
+}
+
+func (s *Span) X() int16 {
+	return int16(s.handle.x)
+}
+
+func (s *Span) Len() uint16 {
+	return uint16(s.handle.len)
+}
+
+func (s *Span) Coverage() byte {
+	return byte(s.handle.coverage)
+}
